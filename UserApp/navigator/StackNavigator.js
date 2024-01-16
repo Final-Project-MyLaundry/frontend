615a1c0042c1f3dbaea77dbcc5f453f7a44d0552
Reza Valovi <rezavalovi98@gmail.com>
2024-01-16 23:04:11 +0700

    layouting register page for user

--- UserApp/navigator/StackNavigator.js
+++ UserApp/navigator/StackNavigator.js
@@ -1,15 +1,10 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { LoginContext } from "../context/LoginContext";
 import LoginForm from "../src/components/Login";
 import RegisterForm from "../src/components/Register";
-// import Home from "../src/components/Home";
-import { useContext } from "react";
-// import LogoutButton from "../src/components/LogoutButton";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
-    // const { isLoggedIn } = useContext(LoginContext);
 
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
